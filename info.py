4ec37b17dbdfacb868eea8f61a30fb71f7348d53
kwicbot78 <115940699+kwicbot78@users.noreply.github.com>
2022-11-28 19:37:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kwicbot:kwicbot@cluster0.mtrrmfd.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "kwicbatch)
+DATABASE_NAME = environ.get('DATABASE_NAME', "kwicbatch")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
